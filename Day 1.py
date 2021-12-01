a8359d7ed23d68c450052c7dfd12690aad2f244a
Eddy Amani <46386577+Ziloxeur@users.noreply.github.com>
2021-12-01 13:56:32 +0100

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -1,3 +1,5 @@
+#First and Second stars in python
+
 nombre = []
 measurements = 0
 
@@ -5,7 +7,7 @@ fichier = open('C:/Users/Eddy Amani/OneDrive/Documents/Advent code/nombre.txt', 
 for i  in fichier.readlines():
     nombre.append(int(i.replace("/n","")))
 
-for i in range(0,1999):
-    if nombre[i] < nombre[i+1]:
+for i in range(0,1997):
+    if nombre[i]+nombre[i+1]+nombre[i+2] < nombre[i+1]+nombre[i+2]+nombre[i+3]:
         measurements += 1
 print(measurements) 
